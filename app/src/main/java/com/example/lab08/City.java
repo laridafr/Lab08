d740eb9d505cd2cbc06da4d71b6ce0fa202bfa58
Fredrik Larida <fredrik.larida@gmail.com>
2025-10-31 15:17:33 -0600

    Added comment

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -11,7 +11,7 @@ public class City {
 
     public String getCityName(){
         return this.city;
-    }
+    } // Making a comment
 
     public String getProvinceName(){
         return this.province;
